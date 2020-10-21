1899829c531f0a0b1b9ea3b1efcdfd04b283a4cc
Octavian Regatun <31423936+octavian-regatun@users.noreply.github.com>
2020-10-21 16:28:51 +0300

    add: Calendar to Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
+import { Container, Grid } from '@material-ui/core';
 import React from 'react';
 import Sidebar from '../components/Sidebar';
+import Calendar from '../components/Calendar';
 
 class Home extends React.PureComponent {
   render() {
     return (
-      <div style={{ height: '100vh' }}>
+      <div style={{ display: 'inline-flex', height: '100vh', width: '100vw' }}>
         <Sidebar color='#171D25' height='100%' width='5rem' />
+
+        <Container
+          maxWidth
+          style={{ height: '100vh', padding: 0, background: '#171D25', overflow: 'auto' }}
+        >
+          <Grid container>
+            <Grid item xs={3}>
+              <Calendar />
+            </Grid>
+          </Grid>
+        </Container>
       </div>
     );
   }
