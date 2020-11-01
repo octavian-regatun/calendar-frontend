3be343426ba9af1d78d812bb1515eb431f086494
Octavian Regatun <31423936+octavian-regatun@users.noreply.github.com>
2020-11-01 02:09:33 +0200

    add: initial theme colors

---
+++ src/utils/theme.js
@@ -0,0 +1,12 @@
+const customTheme = {
+  dark: {
+    primary: {
+      light: '#1e242d',
+      dark: '#171d25'
+    },
+    title: '#ffffff',
+    subtitle: '#bbbdc0'
+  }
+};
+
+export default customTheme;
