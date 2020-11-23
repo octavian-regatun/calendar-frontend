4700907c65ff35ccb110fee8dea5850859ebe877
Octavian Regatun <31423936+octavian-regatun@users.noreply.github.com>
2020-11-23 09:43:20 +0200

    format: App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as axios from 'axios';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import backendURL from './utils/config';
+import Loading from './pages/Loading';
 
-class App extends React.PureComponent {
-  render() {
-    return <Home />;
-  }
+export default function App() {
+  const [loggedIn, setLoggedIn] = useState(undefined);
+
+  useEffect(() => {
+    axios
+      .get(`${backendURL}/auth/loggedIn`, {
+        withCredentials: true
+      })
+      .then((res) => {
+        setLoggedIn(res.data.loggedIn);
+      });
+  });
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          {() => {
+            switch (loggedIn) {
+              case true:
+                return <Home />;
+
+              case false:
+                return <h1>Landing Page</h1>;
+
+              default:
+                return <Loading />;
+            }
+          }}
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
