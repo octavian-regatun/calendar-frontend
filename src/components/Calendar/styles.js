5ab041f243455a445ca188bc7098188c285e84f4
Octavian Regatun <octavian.regatun@outlook.com>
2020-12-09 20:31:48 +0200

    cleanup

--- src/components/Calendar/styles.js
+++ src/components/Calendar/styles.js
@@ -2,8 +2,6 @@ import { makeStyles } from '@material-ui/core';
 import customTheme from '../../utils/theme';
 import './styles.css';
 
-export const calendarStyles = makeStyles({});
-
 export const calendarFooterStyles = makeStyles({
   root: {
     display: 'flex',
