a238f41c38f369380d3e6336525e54b82061263e
Octavian Regatun <octavian.regatun@outlook.com>
2020-12-14 15:56:09 +0200

    cleanup

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,68 +1,68 @@
-import React, { PureComponent } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import { makeStyles } from '@material-ui/core';
 import EventNoteRoundedIcon from '@material-ui/icons/EventNoteRounded';
-import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
 import GroupRoundedIcon from '@material-ui/icons/GroupRounded';
-
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
+import React from 'react';
 import Logo from '../../images/Logo.svg';
 import './Sidebar.css';
 
-const MUIStyles = () => ({});
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  logo: {
+    marginTop: '1rem',
+    width: '5rem'
+  },
+  icon: {
+    width: '2.5rem',
+    height: '2.5rem',
+    color: 'white',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    textAlign: 'center'
+  },
+  iconButton: {
+    width: '5rem',
+    height: '5rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
-class Sidebar extends PureComponent {
-  render() {
-    // const { classes } = this.props;
-    const styles = {
-      container: {
-        width: this.props.width,
-        height: this.props.height,
-        backgroundColor: this.props.color,
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        flexDirection: 'column'
-      },
-      logo: {
-        marginTop: '1rem',
-        width: '5rem'
-      },
-      icon: {
-        width: '2.5rem',
-        height: '2.5rem',
-        color: 'white',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        textAlign: 'center'
-      },
-      iconButton: {
-        width: '5rem',
-        height: '5rem',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }
-    };
-    return (
-      <div style={styles.container}>
-        <img style={styles.logo} src={Logo} alt='logo' />
-        <div>
-          <div style={styles.iconButton} className='iconButton'>
-            <HomeRoundedIcon style={styles.icon} />
-          </div>
-          <div style={styles.iconButton} className='iconButton'>
-            <EventNoteRoundedIcon style={styles.icon} />
-          </div>
-          <div style={styles.iconButton} className='iconButton'>
-            <GroupRoundedIcon style={styles.icon} />
-          </div>
+function Sidebar(props) {
+  const classes = useStyles();
+  return (
+    <div
+      className={`${classes.container} ${props.className}`}
+      style={{
+        height: props.height,
+        width: props.width,
+        backgroundColor: props.color
+      }}
+    >
+      <img className={classes.logo} src={Logo} alt='logo' />
+      <div>
+        <div className={`iconButton ${classes.iconButton}`}>
+          <HomeRoundedIcon className={classes.icon} />
         </div>
-        <div style={styles.iconButton} className='iconButton'>
-          <SettingsRoundedIcon style={styles.icon} />
+        <div className={`iconButton ${classes.iconButton}`}>
+          <EventNoteRoundedIcon className={classes.icon} />
+        </div>
+        <div className={`iconButton ${classes.iconButton}`}>
+          <GroupRoundedIcon className={classes.icon} />
         </div>
       </div>
-    );
-  }
+      <div className={`iconButton ${classes.iconButton}`}>
+        <SettingsRoundedIcon className={classes.icon} />
+      </div>
+    </div>
+  );
 }
 
-export default withStyles(MUIStyles)(Sidebar);
+export default Sidebar;
